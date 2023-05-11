e097d06a459cb5cd7e168220fa8fc86b3dc612ab
renanlaitanobessa <renanbessalaitano@gmail.com>
2023-05-11 15:07:31 -0300

    calculadora

---
+++ Contato.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaSimplesT4
+{
+    internal class Contato
+    {
+        private string primeiroNome;
+        private string sobrenome;
+        private string telefone;
+
+
+
+        public string PrimeiroNome
+        {
+            get { return primeiroNome; }
+            set { primeiroNome = value; }
+        }
+
+
+
+        public string Sobrenome
+        {
+            get { return sobrenome; }
+            set { sobrenome = value; }
+        }
+
+
+
+        public string Telefone
+        {
+            get { return telefone; }
+            set
+            {
+                if (value.Length == 11)
+                    Telefone = value;
+                else
+                    Telefone = "00 00000 0000";
+            }
+        }
+
+        public Contato()
+        {
+            PrimeiroNome = "Jose";
+            Sobrenome = "Da Silva";
+            Telefone = "11-91234-5678"; 
+        }
+
+        public Contato(string primeiroNome, string sobrenome, string telefone)
+        {
+            PrimeiroNome = primeiroNome;
+            Sobrenome = sobrenome;
+            Telefone = telefone;
+
+        }
+
+        public override string ToString()
+        {
+            string saida = String.Empty;
+            saida += String.Format("{0}, {1}" , PrimeiroNome, Sobrenome );
+            saida += String.Format("{0}-{1}-{2}", Telefone.Substring(0, 1), Telefone.Substring(2, 4), Telefone.Substring(7, 3);   
+
+            return saida; 
+
+        }
+
+
+
+    }
+}
+
---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace AgendaSimplesT4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnaddcontato = new System.Windows.Forms.Button();
+            this.txtnome = new System.Windows.Forms.TextBox();
+            this.txtsobrenome = new System.Windows.Forms.TextBox();
+            this.txttelefone = new System.Windows.Forms.TextBox();
+            this.lstContatos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "sobrenome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "telefone";
+            // 
+            // btnaddcontato
+            // 
+            this.btnaddcontato.Location = new System.Drawing.Point(41, 181);
+            this.btnaddcontato.Name = "btnaddcontato";
+            this.btnaddcontato.Size = new System.Drawing.Size(142, 57);
+            this.btnaddcontato.TabIndex = 3;
+            this.btnaddcontato.Text = "inserir contato";
+            this.btnaddcontato.UseVisualStyleBackColor = true;
+            this.btnaddcontato.Click += new System.EventHandler(this.btnaddcontato_Click);
+            // 
+            // txtnome
+            // 
+            this.txtnome.Location = new System.Drawing.Point(99, 36);
+            this.txtnome.Name = "txtnome";
+            this.txtnome.Size = new System.Drawing.Size(100, 20);
+            this.txtnome.TabIndex = 4;
+            // 
+            // txtsobrenome
+            // 
+            this.txtsobrenome.Location = new System.Drawing.Point(99, 75);
+            this.txtsobrenome.Name = "txtsobrenome";
+            this.txtsobrenome.Size = new System.Drawing.Size(100, 20);
+            this.txtsobrenome.TabIndex = 5;
+            // 
+            // txttelefone
+            // 
+            this.txttelefone.Location = new System.Drawing.Point(99, 126);
+            this.txttelefone.Name = "txttelefone";
+            this.txttelefone.Size = new System.Drawing.Size(100, 20);
+            this.txttelefone.TabIndex = 6;
+            // 
+            // lstContatos
+            // 
+            this.lstContatos.FormattingEnabled = true;
+            this.lstContatos.Location = new System.Drawing.Point(335, 36);
+            this.lstContatos.Name = "lstContatos";
+            this.lstContatos.Size = new System.Drawing.Size(225, 199);
+            this.lstContatos.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 450);
+            this.Controls.Add(this.lstContatos);
+            this.Controls.Add(this.txttelefone);
+            this.Controls.Add(this.txtsobrenome);
+            this.Controls.Add(this.txtnome);
+            this.Controls.Add(this.btnaddcontato);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnaddcontato;
+        private System.Windows.Forms.TextBox txtnome;
+        private System.Windows.Forms.TextBox txtsobrenome;
+        private System.Windows.Forms.TextBox txttelefone;
+        private System.Windows.Forms.ListBox lstContatos;
+    }
+}
+
